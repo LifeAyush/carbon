c9351c246b6e44af045816ea68002e243ab4fbd6
Ayush Patil <97448401+LifeAyush@users.noreply.github.com>
2024-01-20 16:56:24 +0530

    stats and footer comps

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./home.css"
+import Navbar from "../../Components/Navbar/Navbar";
+import Stats from "../../Components/Stats/Stats";
+import Footer from "../../Components/Footer/Footer";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+      <Stats />
+      <Footer />
+    </div>
   )
 }
 
