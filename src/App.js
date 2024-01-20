964fc074efeed6ecfa3632a60ac8d0b0bf2c0c13
Ayush Patil <97448401+LifeAyush@users.noreply.github.com>
2024-01-20 13:38:58 +0530

    routing and folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Services from "./Pages/Services/Services";
+import Resources from "./Pages/Resources/Resources";
+import AboutUs from "./Pages/AboutUs/AboutUs";
+import Careers from "./Pages/Career/Career";
+import Partners from "./Pages/Partners/Partners";
 
 function App() {
   return (
     <div className="App">
-      App
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/services" element={<Services />} />
+          <Route exact path="/resources" element={<Resources />} />
+          <Route exact path="/about-us" element={<AboutUs />} />
+          <Route exact path="/partners" element={<Partners />} />
+          <Route exact path="/careers" element={<Careers />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Resources/Resources.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./resources.css"
+
+const Resources = () => {
+  return (
+    <div>Resources</div>
+  )
+}
+
+export default Resources
