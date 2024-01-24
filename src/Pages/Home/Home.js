2d12f5492587934ce3f65f12f1118ce962ec40ac
Ayush Patil <97448401+LifeAyush@users.noreply.github.com>
2024-01-24 22:53:09 +0530

    our journery section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,12 +5,14 @@ import Stats from "../../Components/Stats/Stats";
 import Footer from "../../Components/Footer/Footer";
 import Hero from '../../Components/Hero/Hero';
 import OurServices from '../../Components/OurServices/OurServices';
+import Journey from '../../Components/Journey/Journey';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Journey />
       <Stats />
       <OurServices />
       <Footer /> 
