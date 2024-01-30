d9362cd01419b7436c46f192ad71e32a8b54ac01
Ayush Patil <97448401+LifeAyush@users.noreply.github.com>
2024-01-30 11:43:53 +0530

    Navbar fixed and font fix

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import "./home.css"
+import React from "react";
+import "./home.css";
 import Navbar from "../../Components/Navbar/Navbar";
 import Stats from "../../Components/Stats/Stats";
 import Footer from "../../Components/Footer/Footer";
-import Hero from '../../Components/Hero/Hero';
-import OurServices from '../../Components/OurServices/OurServices';
-import Journey from '../../Components/Journey/Journey';
+import Hero from "../../Components/Hero/Hero";
+import OurServices from "../../Components/OurServices/OurServices";
+import Journey from "../../Components/Journey/Journey";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <Navbar />
-      <Hero />
-      <Journey />
-      <Stats />
-      <OurServices />
-      <Footer /> 
+      <div className="home-content">
+        <Hero />
+        <Journey />
+        <Stats />
+        <OurServices />
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
