f343b5d2ffd75cacebe74edf64eac9440f9d0cae
Prabhjot Singh <singh.74.prabh@gmail.com>
2024-03-02 23:28:57 +0530

    journey component

--- src/Components/Journey/Journey.js
+++ src/Components/Journey/Journey.js
@@ -1,50 +1,38 @@
-import React from "react";
-import "./journey.css";
-import journey1 from "../../Assets/Images/journey1.png";
-import journey2 from "../../Assets/Images/journey2.png";
-import journey3 from "../../Assets/Images/journey3.png";
-import JourneySlider from "./JourneySlider";
+import React, { useEffect, useRef } from "react"
+import "./journey.css"
+import JourneySlider from "./JourneySlider"
 
 const Journey = () => {
-  return (
-    <div className="journey">
-      <div className="journey-content">
-        <span className="journey-content-subtitle">Your U-turn to</span>
-        <span className="journey-content-title">Net Zero Journey</span>
-      </div>
-      {/* <JourneySlider /> */}
-      <div className="journey-carousel">
-        <div className="journey-carousel-card-primary-border">
-          <div className="journey-carousel-card-primary">
-            <img
-              src={journey1}
-              alt="journey1"
-              className="journey-carousel-card-img"
-            />
-            <div className="journey-carousel-card-mask" />
-            <div className="journey-carousel-card-para">
-              We fuel green initiatives for individuals and businesses,
-              accelerating their path to Net Zero.
-            </div>
-          </div>
+    const cont = useRef(null)
+    useEffect(() => {
+        const slider = document.querySelectorAll(".journey-slider")[0]
+        console.log(slider)
+        const transform = (elem) => {
+            const offset = elem.parentElement.offsetTop
+            console.log(offset)
+            let percentage =
+                ((window.scrollY - offset) / window.innerHeight) * 100
+            slider.style.transform = `translate3d(${-percentage}vw, 0, 0)`
+        }
+        window.addEventListener("scroll", (e) => {
+            transform(cont.current)
+        })
+    }, [])
+    return (
+        <div className="journey">
+            <section className="journey-slider-container" ref={cont}>
+                <div className="journey-content">
+                    <span className="journey-content-subtitle">
+                        Your U-turn to
+                    </span>
+                    <span className="journey-content-title">
+                        Net Zero Journey
+                    </span>
+                </div>
+                <JourneySlider />
+            </section>
         </div>
-        <div className="journey-carousel-card-secondary">
-          <img
-            src={journey2}
-            alt="journey2"
-            className="journey-carousel-card-img"
-          />
-        </div>
-        <div className="journey-carousel-card-tertiary">
-          <img
-            src={journey3}
-            alt="journey3"
-            className="journey-carousel-card-img"
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Journey;
+export default Journey
--- src/Components/Journey/JourneySlider.js
+++ src/Components/Journey/JourneySlider.js
@@ -1,153 +1,38 @@
-import React, { useCallback, useEffect, useRef } from "react";
-import "./styles.css";
-import "./journey.css";
-// import Carousel from "react-elastic-carousel";
-import journey1 from "../../Assets/Images/journey1.png";
-import journey2 from "../../Assets/Images/journey2.png";
-import journey3 from "../../Assets/Images/journey3.png";
+import "./journeySlider.css"
 
-const JourneySlider = () => {
-  const wheeling = useRef(false);
-  const wheelingTimeout = useRef(false);
-  const slider = useRef(null);
-  const sliderContainer = useRef(null);
-  const currentIndex = useRef(0);
-  const IMAGES_LENGTH = 4;
+function Card({ image, text }) {
+    return (
+        <section
+            className="journey-card"
+            style={{
+                background: `url(${image})`,
+            }}
+        >
+            <div className="overlay"></div>
+            <p className="card-content">{text}</p>
+        </section>
+    )
+}
 
-  const getNextCurrentIndex = (index) => {
-    if (index === IMAGES_LENGTH) {
-      return index;
-    }
-
-    return index + 1;
-  };
-
-  const getPrevCurrentIndex = (index) => {
-    if (index === 0) {
-      return index;
-    }
-
-    return index - 1;
-  };
-
-  const shouldPreventDefault = useCallback(({ isNext }) => {
-    if (!isNext && currentIndex.current > 0) {
-      return true;
-    }
-
-    if (isNext && currentIndex.current < 4) {
-      return true;
-    }
-
-    return false;
-  }, []);
-
-  const scroll = useCallback(
-    (e) => {
-      const isNext = e.wheelDelta > 0;
-      if (shouldPreventDefault({ isNext })) {
-        e.preventDefault();
-      }
-      if (slider === null) return null;
-
-      clearTimeout(wheelingTimeout.current);
-      wheelingTimeout.current = setTimeout(function () {
-        wheeling.current = false;
-      }, 100);
-
-      if (wheeling.current === true) {
-        return null;
-      }
-
-      if (isNext) {
-        slider.current.slideNext();
-        currentIndex.current = getNextCurrentIndex(currentIndex.current);
-      } else {
-        slider.current.slidePrev();
-        currentIndex.current = getPrevCurrentIndex(currentIndex.current);
-      }
-      wheeling.current = true;
-    },
-    [slider]
-  );
-
-  const stopWheeling = useCallback(() => {
-    wheeling.current = false;
-  }, []);
-
-  useEffect(() => {
-    if (!sliderContainer.current) {
-      return null;
-    }
-    const sliderContainerCurrent = sliderContainer.current;
-    sliderContainerCurrent.addEventListener("wheel", scroll, true);
-
-    return () => {
-      sliderContainerCurrent.removeEventListener("wheel", scroll, false);
-    };
-  }, [scroll, stopWheeling]);
-  let resetTimeout;
-
-  return (
-    <>New Developer Now</>
-    // <div ref={sliderContainer} className="tenants-middle">
-    //   <Carousel
-    //     onScroll={scroll}
-    //     itemsToShow={3}
-    //     ref={slider}
-    //     pagination={false}
-    //     showArrows={false}
-    //     onNextEnd={({ index }) => {
-    //       clearTimeout(resetTimeout);
-    //       if (index + 1 === 4) {
-    //         resetTimeout = setTimeout(() => {
-    //           slider.current.goTo(0);
-    //         }, 1500); // same time
-    //       }
-    //     }}
-    //     className="carousel-flex"
-    //   >
-    //     <div className="journey-carousel-card-primary">
-    //       <img
-    //         src={journey1}
-    //         alt="journey1"
-    //         className="journey-carousel-card-img"
-    //       />
-    //       <div className="journey-carousel-card-mask" />
-    //       <div className="journey-carousel-card-para">
-    //         We fuel green initiatives for individuals and businesses,
-    //         accelerating their path to Net Zero.
-    //       </div>
-    //     </div>
-    //     <div className="journey-carousel-card-secondary">
-    //       <img
-    //         src={journey2}
-    //         alt="journey2"
-    //         className="journey-carousel-card-img"
-    //       />
-    //     </div>
-    //     <div className="journey-carousel-card-tertiary">
-    //       <img
-    //         src={journey3}
-    //         alt="journey3"
-    //         className="journey-carousel-card-img"
-    //       />
-    //     </div>
-    //     <div className="journey-carousel-card-tertiary">
-    //       <img
-    //         src={journey3}
-    //         alt="journey3"
-    //         className="journey-carousel-card-img"
-    //       />
-    //     </div>
-    //     {/* <img
-    //       src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
-    //       alt="new"
-    //       className="secondary-card"
-    //     /> */}
-    //   </Carousel>
-    // </div>
-  );
-};
-
-export default JourneySlider;
+export default function JourneySlider() {
+    return (
+        <section className="journey-slider">
+            <Card
+                text="We fuel green initiatives for individuals and businesses, accelerating their path to Net Zero."
+                image="/Images/Journey/journey1.png"
+            />
+            <Card
+                text="We fuel green initiatives for individuals and businesses, accelerating their path to Net Zero."
+                image="/Images/Journey/journey2.png"
+            />
+            <Card
+                text="We fuel green initiatives for individuals and businesses, accelerating their path to Net Zero."
+                image="/Images/Journey/journey3.png"
+            />
+            <Card
+                text="We fuel green initiatives for individuals and businesses, accelerating their path to Net Zero."
+                image="/Images/Journey/journey3.png"
+            />
+        </section>
+    )
+}
