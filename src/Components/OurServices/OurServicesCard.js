71e648caf6a0d960a50dcad81238cb090d3a28dc
Prabhjot Singh <singh.74.prabh@gmail.com>
2024-03-06 06:13:09 +0530

    stats section updated, service cards animation change

--- src/Components/OurServices/OurServicesCard.js
+++ src/Components/OurServices/OurServicesCard.js
@@ -9,30 +9,35 @@ const OurServicesCard = ({ data }) => {
       setFlipped(false);
     }, 3000);
   }, [flipped]);
-  
-  return (
-    <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
-      <div className="our-services-card" onClick={() => setFlipped(true)}>
-        <div className="text-ramp" />
-        <div className="hover-mask" />
-        <img
-          src={data.img}
-          alt={data.title}
-          className="our-services-card-bg-img"
-        />
-        <div className="our-services-card-text">{data.title}</div>
-      </div>
 
-      <div className="our-services-card-back" onClick={() => setFlipped(false)}>
-        <div className="back-mask" />
-        <img
-          src={data.img}
-          alt={data.title}
-          className="our-services-card-bg-img"
-        />
-        <div className="our-services-card-backtext">{data.para}</div>
-      </div>
-    </ReactCardFlip>
+  return (
+    <section className="service-card">
+      <section className="service-card-inner">
+        <div className="our-services-card" onClick={() => setFlipped(true)}>
+          <div className="text-ramp" />
+          <div className="hover-mask" />
+          <img
+            src={data.img}
+            alt={data.title}
+            className="our-services-card-bg-img"
+          />
+          <div className="our-services-card-text">{data.title}</div>
+        </div>
+
+        <div
+          className="our-services-card-back"
+          onClick={() => setFlipped(false)}
+        >
+          <div className="back-mask" />
+          <img
+            src={data.img}
+            alt={data.title}
+            className="our-services-card-bg-img"
+          />
+          <div className="our-services-card-backtext">{data.para}</div>
+        </div>
+      </section>
+    </section>
   );
 };
 
--- src/Components/Stats/Stats.js
+++ src/Components/Stats/Stats.js
@@ -9,46 +9,38 @@ import stats5 from "../../Assets/Images/stats-5.svg";
 import bg from "../../Assets/Images/statsbg.png";
 
 const Stats = () => {
-
-
-const sData = [
-  {
-    id: 1,
-    img: stats1,
-    name: "0.5-1 m",
-    val: "Rise in global sea level ",
-   
-  },
-  {
-    id: 2,
-    img: stats2,
-    name: "$ 1.6 trillion",
-    val: "Damage by river flooding",
- 
-  },
-  {
-    id: 3,
-    img: stats3,
-    name: "50%",
-    val: "Increase in CO2 emissions",
- 
-  },
-  {
-    id: 4,
-    img: stats4,
-    name: "15x",
-    val: "Increase in death rate due to extreme weather events",
- 
-  },
-  {
-    id: 5,
-    img: stats5,
-    name: "2-3 °C",
-    val: "Rise in temperature as early as 2030",
-
-  },
-];
-
+  const sData = [
+    {
+      id: 1,
+      img: stats1,
+      name: "0.5-1 m",
+      val: "Rise in global sea level ",
+    },
+    {
+      id: 2,
+      img: stats2,
+      name: "$ 1.6 trillion",
+      val: "Damage by river flooding",
+    },
+    {
+      id: 3,
+      img: stats3,
+      name: "50%",
+      val: "Increase in CO2 emissions",
+    },
+    {
+      id: 4,
+      img: stats4,
+      name: "15x",
+      val: "Increase in death rate due to extreme weather events",
+    },
+    {
+      id: 5,
+      img: stats5,
+      name: "2-3 °C",
+      val: "Rise in temperature as early as 2030",
+    },
+  ];
 
   return (
     <>
@@ -62,15 +54,16 @@ const sData = [
           height: "100vh",
         }}
       >
-      <div className="main-2">
-      <div className="heading-stats">
-        <h1> We Aim To Reduce</h1>
-      </div>
-        <div className="card-grid">
-          {sData.map((group, id) => {
-            return <Scard key={id} data={group} />;
-          })}
-        </div></div>
+        <div className="main-2">
+          <div className="heading-stats">
+            <h1> We are committed to migrating</h1>
+          </div>
+          <div className="card-grid">
+            {sData.map((group, id) => {
+              return <Scard key={id} data={group} />;
+            })}
+          </div>
+        </div>
       </div>
     </>
   );
